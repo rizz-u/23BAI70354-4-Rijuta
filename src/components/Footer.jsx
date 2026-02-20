793213ba2839fc204b5a5c3a43e4c997e090a77f
rizz-u <rijutasharma.official@gmail.com>
2026-02-20 18:00:31 +0530

    Update Footer.jsx
    Added dark mode support to the Footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
 export default function Footer() {
+  const { darkMode } = useContext(AppContext);
+
   return (
-    <div className="bg-body-secondary text-center p-3">
+    <div
+      className={`text-center p-3 ${
+        darkMode ? "bg-dark text-light" : "bg-light text-dark"
+      }`}
+    >
       © 2026 CU Fest | Chandigarh University
     </div>
   );
